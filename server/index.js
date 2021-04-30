de24d623dfdc0e77cc8ed6259ea8e0f5354e4018
Mark Liu <markliu202@gmail.com>
2021-04-30 14:38:15 -0400

    user login, signup, get, delete done

--- server/index.js
+++ server/index.js
@@ -23,15 +23,15 @@ app.get("/api/users", async (req, res) => {
 
 // get single user
 app.get("/api/users/:id", async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   const user = await pool.query(`SELECT * FROM users WHERE user_id = $1`, [id]);
   res.json(user.rows[0]);
 });
 
 // handle signup (create user)
 app.post("/api/signup", async (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
+  const username = req.body.username;
+  const password = req.body.password;
   const newUser = await pool.query(
     `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`,
     [username, password]
@@ -46,26 +46,30 @@ app.post("/api/signup", async (req, res) => {
 });
 
 // handle login
-// NOT DONE
 app.post("/api/login", async (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
-  const newUser = await pool.query(
-    `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`,
+  const username = req.body.username;
+  const password = req.body.password;
+  const findUser = await pool.query(
+    `SELECT * FROM users WHERE username = $1 AND password = $2`,
     [username, password]
   );
-  const user = newUser.rows[0];
-  // generate new token
-  jwt.sign({ user: user }, "secretkey", (err, token) => {
+  const user = findUser.rows[0];
+  if (!user) {
     res.json({
-      token: token,
+      message: "user not found",
     });
-  });
+  } else {
+    jwt.sign({ user: user }, "secretkey", (err, token) => {
+      res.json({
+        token: token,
+      });
+    });
+  }
 });
 
 // handle delete user
 app.delete("/api/users/:id", async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   const deletedUser = await pool.query(`DELETE FROM users WHERE user_id = $1`, [
     id,
   ]);
