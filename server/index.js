674c95c0268b7a568d7c94fb83ef416567439bcc
Mark Liu <markliu202@gmail.com>
2021-04-30 14:27:34 -0400

    most crud opereations on users done

--- server/index.js
+++ server/index.js
@@ -12,15 +12,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("wahoo");
+  res.send("wassupp yo you reached the api");
 });
 
 // get all users
-app.post("/api/users", async (req, res) => {
+app.get("/api/users", async (req, res) => {
   const allUsers = await pool.query(`SELECT * FROM users`);
-  res.json(allTodos.rows);
+  res.json(allUsers.rows);
 });
 
+// get single user
+app.get("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await pool.query(`SELECT * FROM users WHERE user_id = $1`, [id]);
+  res.json(user.rows[0]);
+});
+
+// handle signup (create user)
+app.post("/api/signup", async (req, res) => {
+  const { username } = req.body;
+  const { password } = req.body;
+  const newUser = await pool.query(
+    `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`,
+    [username, password]
+  );
+  const user = newUser.rows[0];
+  // generate new token
+  jwt.sign({ user: user }, "secretkey", (err, token) => {
+    res.json({
+      token: token,
+    });
+  });
+});
+
+// handle login
+// NOT DONE
 app.post("/api/login", async (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
@@ -29,11 +55,21 @@ app.post("/api/login", async (req, res) => {
     [username, password]
   );
   const user = newUser.rows[0];
+  // generate new token
   jwt.sign({ user: user }, "secretkey", (err, token) => {
     res.json({
       token: token,
     });
   });
+});
+
+// handle delete user
+app.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await pool.query(`DELETE FROM users WHERE user_id = $1`, [
+    id,
+  ]);
+  res.json({ id: id });
 });
 
 const PORT = process.env.PORT || 5000;
